d78bbc918e6042ce8a96b1f1c985c73e21e16383
shivrjpatil <shivrajp972@gmail.com>
2022-12-21 18:57:45 +0530

    started gameplay with playerposition

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int playerPosition=0;
+            Console.WriteLine("lets play! \nPlayerPosition=" +playerPosition);
+            Console.ReadLine();
+        }
+    }
+}
